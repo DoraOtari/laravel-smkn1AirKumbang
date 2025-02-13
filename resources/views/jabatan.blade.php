2f38f10884045ba0c8207b9bc47095f09b2e8a23
Setiawan <setiawan@gmail.com>
2025-02-13 14:29:14 +0700

    13/02 part2

---
+++ resources/views/jabatan.blade.php
@@ -0,0 +1,32 @@
+@extends('tema')
+@section('konten')
+<div class="col-6" >
+    <div class="card">
+        <div class="card-body">
+            <h3 class="card-title">Data Jabatan</h3>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Nama Jabatan</th>
+                        <th>Gaji Jabatan</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>    
+
+
+
+
+
+
+
+
+@endsection
