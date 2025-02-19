9b41323232f84efde19385a60404f848809792ec
Setiawan <setiawan@gmail.com>
2025-02-19 14:42:10 +0700

    19 pt2

--- resources/views/tambah_jabatan.blade.php
+++ resources/views/tambah_jabatan.blade.php
@@ -9,11 +9,14 @@
                             @csrf
                             <div class="mb-3">
                                 <label for="nama_jabatan" class="form-label">Nama Jabatan</label>
-                                <input type="text" name="nama_jabatan" id="nama_jabatan" class="form-control">
+                                <input placeholder="masukan nama jabatan" type="text" name="nama_jabatan" id="nama_jabatan" class="form-control">
                             </div>
                             <div class="mb-3">
                                 <label for="gaji_jabatan" class="form-label">Gaji Jabatan</label>
-                                <input type="number" name="gaji_jabatan" id="gaji_jabatan" class="form-control">
+                                <input placeholder="masukan gaji jabatan" type="number" name="gaji_jabatan" id="gaji_jabatan" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <button type="submit" class="btn btn-success col-12">Submit</button>
                             </div>
                         </form>
                     </div>
